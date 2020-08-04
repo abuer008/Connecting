d252d6ad992d542435afc35c090bff42cedcc34f
boweixiao <x.bowei@aol.com>
2020-08-04 20:00:28 +0200

    Initial Commit

---
+++ Connecting/ConnectingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectingApp.swift
+//  Connecting
+//
+//  Created by bowei xiao on 04.08.20.
+//
+
+import SwiftUI
+
+@main
+struct ConnectingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Connecting/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Connecting
+//
+//  Created by bowei xiao on 04.08.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
